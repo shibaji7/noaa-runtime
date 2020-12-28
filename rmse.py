ebb5e0ce1eeae43f0cad6bb3bdefee706ab6a064
Shibaji Chakraborty <shibaji7@vt.edu>
2020-12-28 20:53:54 +0000

    Datastruct

--- rmse.py
+++ rmse.py
@@ -1,17 +1,45 @@
+import os
+import sys
 import pandas as pd
 import datetime as dt
 import numpy as np
 
-sub = pd.read_csv("submission/submission.csv")
-sub.timedelta = pd.to_timedelta(sub.timedelta)
+import argparse
+import dateutil
+import json
 
-dst = pd.read_csv("data/dst_labels.csv")
-dst.timedelta = pd.to_timedelta(dst.timedelta)
 
-print(" Length (before):", len(dst), len(sub))
-new_df = sub.merge(dst,  how='inner', left_on=["period","timedelta"], right_on = ["period","timedelta"])
-print(" Length (after):", len(new_df))
-print(new_df.head())
-print(" RMSE(t0) - ", (np.sqrt(np.mean((new_df.dst-new_df.t0)**2))))
-dst_t1, t1 = np.roll(new_df.dst, -1)[:-1], np.array(new_df.t1)[:-1]
-print(" RMSE(t1) - ", (np.sqrt(np.mean((dst_t1-t1)**2))))
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a", "--algo", required=True, help="Algorithm names [LR, LSTM, XGB, GB]")
+    parser.add_argument("-t", "--ds", required=True, help="Dataset name [2h, 6h, 24h, 72h]")
+    parser.add_argument("-d", "--desc", default="Algorithm description", help="Description in terms of training")
+    args = parser.parse_args()
+
+    os.system("mkdir -p RMSE_estimates_local/{algo}/{ds}/".format(algo=args.algo, ds=algo.ds))
+    os.system("cp submission/submission.csv RMSE_estimates_local/{algo}/{ds}/".format(algo=args.algo, ds=algo.ds))
+    
+    sub = pd.read_csv("submission/submission.csv")
+    sub.timedelta = pd.to_timedelta(sub.timedelta)
+    
+    dst = pd.read_csv("data/dst_labels.csv")
+    dst.timedelta = pd.to_timedelta(dst.timedelta)
+    
+    print(" Length (before):", len(dst), len(sub))
+    new_df = sub.merge(dst,  how='inner', left_on=["period","timedelta"], right_on = ["period","timedelta"])
+    print(" Length (after):", len(new_df))
+    print(" RMSE(t0) - ", (np.sqrt(np.mean((new_df.dst-new_df.t0)**2))))
+    dst_t1, t1 = np.roll(new_df.dst, -1)[:-1], np.array(new_df.t1)[:-1]
+    print(" RMSE(t1) - ", (np.sqrt(np.mean((dst_t1-t1)**2))))
+    
+    rdme = {
+        "desc": args.desc,
+        "ds": args.ds,
+        "algo": args.algo,
+        "date": dt.datetime.now().strftime("%Y-%m-%d %H:%M"),
+        "RMSE(t0)": "%.4f"%(np.sqrt(np.mean((new_df.dst-new_df.t0)**2))),
+        "RMSE(t1)": "%.4f"%(np.sqrt(np.mean((dst_t1-t1)**2)))
+    }
+    with open("RMSE_estimates_local/{algo}/{ds}/README.json".format(algo=args.algo, ds=algo.ds), "w") as f:
+        o = json.dumps(rdme, sort_keys=True, indent=4)
+        f.write(o)
